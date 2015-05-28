cb2aa709e34afab645eeae6ba9da1ba593123b1d
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2015-05-28 11:01:46 +0300

    add code for toString test

--- tests/FlashTest.php
+++ tests/FlashTest.php
@@ -3,166 +3,179 @@ session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-class FlashTest extends PHPUnit_Framework_TestCase {
+class FlashTest extends PHPUnit_Framework_TestCase
+{
 
-  /** @test */
-  public function testStaticCall()
-  {
-    \Tamtamchik\Flash\Flash::message('Static message');
+    /** @test */
+    public function testStaticCall()
+    {
+        \Tamtamchik\Flash\Flash::message('Static message');
 
-    $this->assertNotEmpty(\Tamtamchik\Flash\Flash::display());
-  }
+        $this->assertNotEmpty(\Tamtamchik\Flash\Flash::display());
+    }
 
-  /** @test */
-  public function testCreation()
-  {
-    $flash = new \Tamtamchik\Flash\Flash();
+    /** @test */
+    public function testCreation()
+    {
+        $flash = new \Tamtamchik\Flash\Flash();
 
-    $this->assertFalse($flash->hasMessages());
-    $this->assertEquals('Tamtamchik\Flash\Flash', get_class($flash));
-  }
+        $this->assertFalse($flash->hasMessages());
+        $this->assertEquals('Tamtamchik\Flash\Flash', get_class($flash));
+    }
 
-  /** @test */
-  public function testFunction()
-  {
-    $flash = flash();
+    /** @test */
+    public function testFunction()
+    {
+        $flash = flash();
 
-    $this->assertFalse($flash->hasMessages());
-    $this->assertEquals('Tamtamchik\Flash\Flash', get_class($flash));
-  }
+        $this->assertFalse($flash->hasMessages());
+        $this->assertEquals('Tamtamchik\Flash\Flash', get_class($flash));
+    }
 
-  /** @test */
-  public function testMessageWorkflow()
-  {
-    $flash = flash('Test info message');
+    /** @test */
+    public function testMessageWorkflow()
+    {
+        $flash = flash('Test info message');
 
-    $this->assertTrue($flash->hasMessages());
-    $this->assertContains('Test info message', $flash->display());
-    $this->assertFalse($flash->hasMessages());
-  }
+        $this->assertTrue($flash->hasMessages());
+        $this->assertContains('Test info message', $flash->display());
+        $this->assertFalse($flash->hasMessages());
+    }
 
-  /** @test */
-  public function testFunctionMessageType()
-  {
-    $flash = flash('Test info message', 'success');
+    /** @test */
+    public function testFunctionMessageType()
+    {
+        $flash = flash('Test info message', 'success');
 
-    $this->assertContains('success', $flash->display());
-  }
+        $this->assertContains('success', $flash->display());
+    }
 
-  /** @test */
-  public function testChaining()
-  {
-    $flash = flash()->message('Test info message 1')->message('Test info message 2');
+    /** @test */
+    public function testChaining()
+    {
+        $flash = flash()->message('Test info message 1')->message('Test info message 2');
 
-    $content = $flash->display();
-    $this->assertContains('Test info message 1', $content);
-    $this->assertContains('Test info message 2', $content);
-  }
+        $content = $flash->display();
+        $this->assertContains('Test info message 1', $content);
+        $this->assertContains('Test info message 2', $content);
+    }
 
-  /** @test */
-  public function testInfoDefaultMessage()
-  {
-    $flash = flash('Test info message');
+    /** @test */
+    public function testInfoDefaultMessage()
+    {
+        $flash = flash('Test info message');
 
-    $this->assertContains('info', $flash->display());
-  }
+        $this->assertContains('info', $flash->display());
+    }
 
-  /** @test */
-  public function testMessageTypes()
-  {
-    $flash = flash()
-      ->message('Dummy 1', 'success')
-      ->message('Dummy 2', 'info')
-      ->message('Dummy 2', 'warning')
-      ->message('Dummy 2', 'error');
+    /** @test */
+    public function testMessageTypes()
+    {
+        $flash = flash()
+            ->message('Dummy 1', 'success')
+            ->message('Dummy 2', 'info')
+            ->message('Dummy 2', 'warning')
+            ->message('Dummy 2', 'error');
 
-    $content = $flash->display();
-    $this->assertContains('success', $content);
-    $this->assertContains('info', $content);
-    $this->assertContains('success', $content);
-    $this->assertContains('danger', $content);
-  }
+        $content = $flash->display();
+        $this->assertContains('success', $content);
+        $this->assertContains('info', $content);
+        $this->assertContains('success', $content);
+        $this->assertContains('danger', $content);
+    }
 
-  /** @test */
-  public function testPartialDisplay()
-  {
-    $flash = flash()->message('Dummy 1', 'success')->message('Dummy 2');
+    /** @test */
+    public function testPartialDisplay()
+    {
+        $flash = flash()->message('Dummy 1', 'success')->message('Dummy 2');
 
-    $this->assertTrue($flash->hasMessages('success'));
+        $this->assertTrue($flash->hasMessages('success'));
 
-    $content = $flash->display('success');
+        $content = $flash->display('success');
 
-    $this->assertContains('Dummy 1', $content);
-    $this->assertNotContains('Dummy 2', $content);
-  }
+        $this->assertContains('Dummy 1', $content);
+        $this->assertNotContains('Dummy 2', $content);
+    }
 
-  /** @test */
-  public function testWrongDisplays()
-  {
-    $flash = flash()->message('Dummy 1', 'success')->message('Dummy 2');
+    /** @test */
+    public function testWrongDisplays()
+    {
+        $flash = flash()->message('Dummy 1', 'success')->message('Dummy 2');
 
-    $this->assertFalse($flash->hasMessages('wrong'));
+        $this->assertFalse($flash->hasMessages('wrong'));
 
-    $content = $flash->display('wrong');
+        $content = $flash->display('wrong');
 
-    $this->assertEmpty($content);
-  }
+        $this->assertEmpty($content);
+    }
 
-  /** @test */
-  public function testAccessAsString()
-  {
-    $flash = new \Tamtamchik\Flash\Engine;
-    $flash->clear();
+    /** @test */
+    public function testAccessAsString()
+    {
+        $flash = new \Tamtamchik\Flash\Engine;
+        $flash->clear();
 
-    $flash->message('Test message');
-    $this->assertContains('Test message', "{$flash}");
-  }
+        $flash->message('Test message');
+        $this->assertContains('Test message', "{$flash}");
+    }
 
-  /** @test */
-  public function testWrongMessageType()
-  {
-    $flash = flash();
+    /** @test */
+    public function testWrongMessageType()
+    {
+        $flash = flash();
 
-    $flash->message('Test message', 'bad');
-    $this->assertFalse(flash()->hasMessages());
-  }
+        $flash->message('Test message', 'bad');
+        $this->assertFalse(flash()->hasMessages());
+    }
 
-  /** @test */
-  public function testThatSessionIsShared()
-  {
-    flash('Checking shared');
+    /** @test */
+    public function testThatSessionIsShared()
+    {
+        flash('Checking shared');
 
-    $content = flash()->display();
-    $this->assertContains('Checking shared', $content);
-  }
+        $content = flash()->display();
+        $this->assertContains('Checking shared', $content);
+    }
 
-  /** @test */
-  public function testItFlushesChanges()
-  {
-    flash('First one', 'success')->message('Other one', 'info')->display();
-    flash('Third one', 'error')->display();
+    /** @test */
+    public function testItFlushesChanges()
+    {
+        flash('First one', 'success')->message('Other one', 'info')->display();
+        flash('Third one', 'error')->display();
 
-    $this->assertFalse(flash()->hasMessages());
-  }
+        $this->assertFalse(flash()->hasMessages());
+    }
 
-  /** @test */
-  public function testClearFunction()
-  {
-    flash('I\'ll never see this message', 'success');
-    flash()->clear();
+    /** @test */
+    public function testClearFunction()
+    {
+        flash('I\'ll never see this message', 'success');
+        flash()->clear();
 
-    $this->assertFalse(flash()->hasMessages());
-  }
+        $this->assertFalse(flash()->hasMessages());
+    }
 
-  /** @test */
-  public function testShortcuts()
-  {
-    flash()->error('Info message')->warning('Info message')->info('Info message')->success('Info message');
+    /** @test */
+    public function testShortcuts()
+    {
+        flash()->error('Info message')->warning('Info message')->info('Info message')->success('Info message');
 
-    $content = flash()->display();
-    $this->assertContains('danger', $content);
-    $this->assertContains('warning', $content);
-    $this->assertContains('info', $content);
-    $this->assertContains('success', $content);
-  }
+        $content = flash()->display();
+        $this->assertContains('danger', $content);
+        $this->assertContains('warning', $content);
+        $this->assertContains('info', $content);
+        $this->assertContains('success', $content);
+    }
+
+    /** @test */
+    public function testToString()
+    {
+        flash('Testing toString', 'success');
+        $flash1 = new \Tamtamchik\Flash\Flash;
+        $this->assertContains('toString', (string)$flash1);
+
+        flash('Testing toString', 'success');
+        $flash2 = flash();
+        $this->assertContains('toString', (string)$flash2);
+    }
 }
