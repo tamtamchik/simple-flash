5cf4e6f27ef4e5675973ace1664fd937a0154034
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2015-05-29 09:50:51 +0300

    add @package for function

--- src/function.php
+++ src/function.php
@@ -5,6 +5,8 @@ if (! function_exists('flash')) {
     /**
      * Wrapper for flash object to be used as global function.
      *
+     * @package Tamtamchik\SimpleFlash
+     *
      * @param string $message
      * @param string $type
      *
