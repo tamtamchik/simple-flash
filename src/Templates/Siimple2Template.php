5fc0da268b6640a395f6ccd3c9c12d67f5e6b355
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2017-04-05 15:56:49 +0700

    code cleanup

--- src/Templates/Siimple2Template.php
+++ src/Templates/Siimple2Template.php
@@ -2,31 +2,13 @@
 
 namespace Tamtamchik\SimpleFlash\Templates;
 
-use Tamtamchik\SimpleFlash\BaseTemplate;
 use Tamtamchik\SimpleFlash\TemplateInterface;
 
 /**
  * Class Siimple2Template.
  * Uses default Semantic UI markdown for flash messages.
  */
-class Siimple2Template extends BaseTemplate implements TemplateInterface
+class Siimple2Template extends SiimpleTemplate implements TemplateInterface
 {
-    protected $prefix  = '';
-    protected $postfix = '<br />';
     protected $wrapper = '<div class="siimple-alert siimple-alert--%s">%s</div>';
-
-    /**
-     * Override base function to suite Bootstrap 3 alert naming.
-     *
-     * @param $messages - message text
-     * @param $type     - message type: success, info, warning, error
-     *
-     * @return string
-     */
-    public function wrapMessages($messages, $type)
-    {
-        $type = ($type == 'success') ? 'done' : $type;
-
-        return sprintf($this->getWrapper(), $type, $messages);
-    }
 }
--- src/Templates/Uikit3Template.php
+++ src/Templates/Uikit3Template.php
@@ -2,18 +2,15 @@
 
 namespace Tamtamchik\SimpleFlash\Templates;
 
-use Tamtamchik\SimpleFlash\BaseTemplate;
 use Tamtamchik\SimpleFlash\TemplateInterface;
 
 /**
  * Class Uikit3Template.
  * Uses default Semantic UI markdown for flash messages.
  */
-class Uikit3Template extends BaseTemplate implements TemplateInterface
+class Uikit3Template extends Uikit2Template implements TemplateInterface
 {
-    protected $prefix  = '<p>';
-    protected $postfix = '</p>';
-    protected $wrapper = '<div class="uk-alert uk-alert-%s" data-uk-alert>%s</div>';
+    protected $wrapper = '<div uk-alert class="uk-alert-%s">%s</div>';
 
     /**
      * Override base function to suite Bootstrap 3 alert naming.
@@ -26,6 +23,7 @@ class Uikit3Template extends BaseTemplate implements TemplateInterface
     public function wrapMessages($messages, $type)
     {
         $type = ($type == 'error') ? 'danger' : $type;
+        $type = ($type == 'info') ? 'primary' : $type;
 
         return sprintf($this->getWrapper(), $type, $messages);
     }
