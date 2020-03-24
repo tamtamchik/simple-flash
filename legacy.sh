12466cdae5385b1c0fca3410cdd2dc8e6cd8ab93
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2020-03-24 10:55:27 +0300

    Fix legacy tests

--- legacy.sh
+++ legacy.sh
@@ -7,10 +7,10 @@
 #find tests -name '*.php' -exec sed -i '.bak' 's/assertStringContainsString(/assertContains(/' {} \;
 
 # Convert to legacy version (Linux)
-find tests -name '*.php' -exec sed 's/PHPUnit\\Framework\\TestCase/PHPUnit_Framework_TestCase/' {} \;
-find tests -name '*.php' -exec sed 's/protected function setUp(): void/public function setUp()/' {} \;
-find tests -name '*.php' -exec sed 's/assertStringNotContainsString(/assertNotContains(/' {} \;
-find tests -name '*.php' -exec sed 's/assertStringContainsString(/assertContains(/' {} \;
+find tests -name '*.php' -exec sed -i 's/PHPUnit\\Framework\\TestCase/PHPUnit_Framework_TestCase/' {} \;
+find tests -name '*.php' -exec sed -i 's/protected function setUp(): void/public function setUp()/' {} \;
+find tests -name '*.php' -exec sed -i 's/assertStringNotContainsString(/assertNotContains(/' {} \;
+find tests -name '*.php' -exec sed -i 's/assertStringContainsString(/assertContains(/' {} \;
 
 # Convert to latest version (Mac OS)
 #find tests -name '*.php' -exec sed -i '.bak' 's/PHPUnit_Framework_TestCase/PHPUnit\\Framework\\TestCase/' {} \;
@@ -19,7 +19,7 @@ find tests -name '*.php' -exec sed 's/assertStringContainsString(/assertContains
 #find tests -name '*.php' -exec sed -i '.bak' 's/assertContains(/assertStringContainsString(/' {} \;
 
 # Convert to latest version (Linux)
-#find tests -name '*.php' -exec sed 's/PHPUnit_Framework_TestCase/PHPUnit\\Framework\\TestCase/' {} \;
-#find tests -name '*.php' -exec sed 's/public function setUp()/protected function setUp(): void/' {} \;
-#find tests -name '*.php' -exec sed 's/assertNotContains(/assertStringNotContainsString(/' {} \;
-#find tests -name '*.php' -exec sed 's/assertContains(/assertStringContainsString(/' {} \;
+#find tests -name '*.php' -exec sed -i 's/PHPUnit_Framework_TestCase/PHPUnit\\Framework\\TestCase/' {} \;
+#find tests -name '*.php' -exec sed -i 's/public function setUp()/protected function setUp(): void/' {} \;
+#find tests -name '*.php' -exec sed -i 's/assertNotContains(/assertStringNotContainsString(/' {} \;
+#find tests -name '*.php' -exec sed -i 's/assertContains(/assertStringContainsString(/' {} \;
